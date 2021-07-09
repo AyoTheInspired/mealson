0b1c48124793ba2a7fea19178b765af15fc62963
Ayo <bimboladxtreme@gmail.com>
2021-07-09 15:53:49 +0100

    Changes on mobile section

--- src/components/MobileSection.js
+++ src/components/MobileSection.js
@@ -8,36 +8,38 @@ function MobileSection() {
 		<Container fluid>
 			<Row>
 				<Section className="col d-flex flex-wrap justify-content-center align-items-center py-3 mx-auto">
-					<div className="order-wrap col-lg-5 col-md-5 col-sm-10 mx-auto">
-						<h2 className="order__top mb-3">
-							Order food on the go with the <br /> Mealson App
-						</h2>
+					<SectionWrap className="col mx-auto">
+						<div className="order-wrap col-lg-4 col-md-5 col-sm-10 mx-auto">
+							<h2 className="order__top mb-3">
+								Order food on the go with the <br /> Mealson App
+							</h2>
 
-						<h5 className="order__mid mb-0">
-							Pay with in app wallet, collect loyalty points, <br /> track your
-							orders in real time, and much more.
-						</h5>
+							<h5 className="order__mid mb-0">
+								Pay with in app wallet, collect loyalty points, <br /> track
+								your orders in real time, and much more.
+							</h5>
 
-						<div className="store__wrap col pl-0 flexed mt-4">
+							<div className="store__wrap col pl-0 flexed mt-4">
+								<img
+									src="./images/app-store.png"
+									alt=""
+									className="store__img w-25"
+								/>
+								<img
+									src="./images/gplay-badge.png"
+									alt=""
+									className="store__img w-25 mr-auto"
+								/>
+							</div>
+						</div>
+						<div className="phone-wrap mt-4 flexed col-lg-4 col-md-5 col-sm-10 mx-auto">
 							<img
-								src="./images/app-store.png"
+								src="./images/phone.png"
 								alt=""
-								className="store__img w-25"
-							/>
-							<img
-								src="./images/gplay-badge.png"
-								alt=""
-								className="store__img w-25 mr-auto"
+								className="phone__wrap-img w-75"
 							/>
 						</div>
-					</div>
-					<div className="phone-wrap mt-4 flexed col-lg-5 col-md-5 col-sm-10 mx-auto">
-						<img
-							src="./images/phone.png"
-							alt=""
-							className="phone__wrap-img w-75"
-						/>
-					</div>
+					</SectionWrap>
 				</Section>
 			</Row>
 		</Container>
@@ -101,3 +103,5 @@ export const Section = styled.section`
 		font-weight: bold;
 	}
 `;
+
+const SectionWrap = styled.div``;
